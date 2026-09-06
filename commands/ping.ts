466ec67b63bde385600161e866e5a2f2b034fb20
der16214583-crypto <der16214583@gmail.com>
2026-09-06 03:25:31 +0300

    Delete commands directory

--- commands/ping.ts
+++
@@ -1,9 +0,0 @@
-import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js';
-
-export const data = new SlashCommandBuilder()
-  .setName('ping')
-  .setDescription('Botun çalışıp çalışmadığını kontrol eder');
-
-export async function execute(interaction: ChatInputCommandInteraction) {
-  await interaction.reply('🏓 Pong!');
-}
